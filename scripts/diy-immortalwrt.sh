b93dc3b26eed7c5a842ae5bd1c64952e695f4247
loryncien <lluocheng88@gmail.com>
2024-01-20 01:11:05 +0800

    Update diy-immortalwrt.sh

--- scripts/diy-immortalwrt.sh
+++ scripts/diy-immortalwrt.sh
@@ -1,1 +1,70 @@
+#!/bin/bash
 
+# 移除要替换的包
+rm -rf feeds/luci/applications/luci-app-netdata
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+echo "Add luci-app-netdata"
+# find . -maxdepth 4 -iname "*netdata" -type d | xargs rm -rf
+rm -rf feeds/luci/applications/luci-app-netdata
+git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+ln -s package/luci-app-netdata/po/zh-cn package/luci-app-netdata/po/zh_Hans
+
+echo "Add luci-app-eqosplus"
+git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+sed -i '/"Control"/d' package/luci-app-eqosplus/luasrc/controller/eqosplus.lua
+sed -i 's/10/99/g' package/luci-app-eqosplus/luasrc/controller/eqosplus.lua
+sed -i 's/\"control\"/\"network\"/g' `grep "control" -rl package/luci-app-eqosplus`
+
+echo "Add luci-app-autotimeset"
+find . -maxdepth 4 -iname "*autotimeset" -type d | xargs rm -rf
+git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+
+# Themes
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
+
+# 更改 Argon 主题背景
+# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# 取消主题默认设置
+find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+
+# 修改 Makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+
+# 修复 hostapd 报错
+# cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
+
+# 调整 ZeroTier 到 服务 菜单
+# sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+# sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
+
+# 修改默认IP
+# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 更改默认 Shell 为 zsh
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# TTYD 免登录
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 修改欢迎 banner
+cp -f $GITHUB_WORKSPACE/data/banner package/base-files/files/etc/banner
+
+# samba解除root限制
+sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.template
+
+./scripts/feeds update -a
+./scripts/feeds install -a
