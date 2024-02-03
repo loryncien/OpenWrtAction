3674d645e4037e48f098eb3d4d9810f552d3b1c4
loryncien <lluocheng88@gmail.com>
2024-02-04 02:23:15 +0800

    Update preset-terminal-tools.sh

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -19,20 +19,33 @@ pushd files/root
 # cp $GITHUB_WORKSPACE/data/zsh/.zshrc .
 
 git clone --depth 1 https://github.com/skywind3000/z.lua.git ./.config/z.lua
-curl "https://raw.githubusercontent.com/loryncien/fz.sh/master/fz.sh" -o ./.config/fz.sh
+cat >> ~/.bashrc <<EOP 
+# z.lua
+alias zc='z -c'      # 严格匹配当前路径的子路径
+alias zz='z -i'      # 使用交互式选择模式
+alias zf='z -I'      # 使用 fzf 对多个结果进行选择
+alias zb='z -b'      # 快速回到父目录
+alias zbi='z -b -i'  # 快速回到父目录交互式选择模式
+alias zh='z -i -t .' # 历史路径
+
+# When you are using j xxx it will first try cd xxx and then z xxx if cd failed.
+function j() {
+    if [[ "$argv[1]" == "-"* ]]; then
+        z "$@"
+    else
+        cd "$@" 2> /dev/null || z "$@"
+    fi
+}
+
+eval "\$(lua /root/.config/z.lua/z.lua --init bash once enhanced)"
+
+EOP
 
 ## Install vim-plug
 curl -fLo ./.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp $GITHUB_WORKSPACE/data/bashrc .bashrc
-
-## Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ./.config/fzf
-.config/fzf/install --bin
-cp $GITHUB_WORKSPACE/data/fzf.bash .fzf.bash
-echo '# Note: opkg update && opkg install findutils findutils-find --force-reinstall' >> .bashrc
-echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> .bashrc
+# cp $GITHUB_WORKSPACE/data/bashrc .bashrc
 
 popd
 
